105375958228b588709b6954562124e8c3a88562
Leonardo Bourscheid <leonardojohannbourscheid@gmail.com>
2026-01-24 18:26:30 -0300

    Learned about autoconfiguration report and server configs

---
+++ example_16/src/main/java/com/github/leojbours/spring/ExampleApplication.java
@@ -0,0 +1,16 @@
+package com.github.leojbours.spring;
+
+import javax.sql.DataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
+
+//@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}

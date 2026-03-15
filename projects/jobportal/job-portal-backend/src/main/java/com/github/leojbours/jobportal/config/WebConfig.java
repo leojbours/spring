acfcca2222ac3dac573ce85be07adb92e018cfd2
Leonardo Bourscheid <leonardojohannbourscheid@gmail.com>
2026-03-15 13:15:23 -0300

    Learned about CORS

--- projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/config/WebConfig.java
+++ projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.github.leojbours.jobportal.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +12,24 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void configureApiVersioning(ApiVersionConfigurer configurer) {
-    configurer.addSupportedVersions("1.0.0");
-    configurer.useMediaTypeParameter(MediaType.parseMediaType("application/vnd.leojbours+json"), "v");
+    configurer.useMediaTypeParameter(MediaType.parseMediaType("application/vnd.leojbours+json"), "v")
+              .addSupportedVersions("1.0", "2.0", "3.0")
+              .setDefaultVersion("1.0");
+  }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    configurer.addPathPrefix("/api", ignored -> true);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
   }
 }
--- projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/controller/impl/CompanyControllerImpl.java
+++ projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/controller/impl/CompanyControllerImpl.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/companies")
+@RequestMapping("/companies")
 public class CompanyControllerImpl implements CompanyController {
 
   CompanyService companyService;

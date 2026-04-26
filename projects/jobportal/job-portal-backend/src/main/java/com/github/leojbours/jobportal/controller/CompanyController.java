f56f0eaf310127537073e68f8c8c28bb6d0990c4
Leonardo Bourscheid <leonardojohannbourscheid@gmail.com>
2026-04-26 16:50:22 -0300

    Add swagger for documentation and update controller interfaces

--- projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/controller/CompanyController.java
+++ projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/controller/CompanyController.java
@@ -1,10 +1,27 @@
 package com.github.leojbours.jobportal.controller;
 
 import com.github.leojbours.jobportal.dto.CompanyDto;
-import com.github.leojbours.jobportal.entity.Company;
+import com.github.leojbours.jobportal.service.CompanyService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public interface CompanyController {
-  ResponseEntity<List<CompanyDto>> findAll();
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+  CompanyService companyService;
+
+  @Autowired
+  public CompanyController(CompanyService companyService) {
+    this.companyService = companyService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<CompanyDto>> findAll() {
+    return ResponseEntity.ok(companyService.findAll());
+  }
 }
--- projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/controller/ContactController.java
+++ projects/jobportal/job-portal-backend/src/main/java/com/github/leojbours/jobportal/controller/ContactController.java
@@ -1,8 +1,45 @@
 package com.github.leojbours.jobportal.controller;
 
 import com.github.leojbours.jobportal.dto.ContactRequestDTO;
+import com.github.leojbours.jobportal.service.ContactService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-public interface ContactController {
-  ResponseEntity<String> saveContactMessage(ContactRequestDTO contactRequestDTO);
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+  private final ContactService contactService;
+
+  @Autowired
+  public ContactController(ContactService contactService) {
+    this.contactService = contactService;
+  }
+
+  @PostMapping()
+  public ResponseEntity<String> saveContactMessage(@RequestBody @Valid ContactRequestDTO contactRequestDTO) {
+    boolean saved = contactService.saveContact(contactRequestDTO);
+    if (saved) {
+      return ResponseEntity.status(HttpStatus.CREATED).body("Message sent successfully");
+    } else {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send message");
+    }
+  }
+
+  @GetMapping
+  public ResponseEntity<String> fetchOpenContacts(
+      @RequestParam
+      @NotBlank(message = "Status is required")
+      String status
+  ) {
+    return ResponseEntity.ok("This are the contacts with status: " + status);
+  }
 }
